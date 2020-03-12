acf05693d860290490d077c6dd74601946e95178
Johannes Kastl <kastl@b1-systems.de>
2020-03-12 16:48:45 +0100

    10_add_security_group.tf: add rules to allow pings (for both IPv4 and IPv6) automatically. Who could work without ping?

--- 10_add_security_group.tf
+++ 10_add_security_group.tf
@@ -1,10 +1,34 @@
 #
-
+# create security group
+#
 resource "openstack_networking_secgroup_v2" "create_security_group" {
   name                 = var.security_group_name
   description          = var.security_group_description
 }
 
+#
+# allow ICMP (to have working pings...) on IPv4
+#
+resource "openstack_networking_secgroup_rule_v2" "security_group_rule_icmp_ipv4" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  security_group_id = openstack_networking_secgroup_v2.create_security_group.id
+}
+
+#
+# allow ICMP (to have working pings...) on IPv6
+#
+resource "openstack_networking_secgroup_rule_v2" "security_group_rule_icmp_ipv6" {
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "icmp"
+  security_group_id = openstack_networking_secgroup_v2.create_security_group.id
+}
+
+#
+# create rules defined by the user
+#
 resource "openstack_networking_secgroup_rule_v2" "security_group_rule_nodes" {
 
     for_each = var.security_group_rules
