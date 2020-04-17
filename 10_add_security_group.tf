e6476029bbcfb6b878b913bb1d1a9dcb6288d0df
Johannes Kastl <kastl@b1-systems.de>
2020-04-17 19:34:29 +0200

    10_add_security_group.tf: fix indentation to make 'terraform fmt' happy

--- 10_add_security_group.tf
+++ 10_add_security_group.tf
@@ -2,8 +2,8 @@
 # create security group
 #
 resource "openstack_networking_secgroup_v2" "create_security_group" {
-  name                 = var.security_group_name
-  description          = var.security_group_description
+  name        = var.security_group_name
+  description = var.security_group_description
 }
 
 #
@@ -39,13 +39,13 @@ resource "openstack_networking_secgroup_rule_v2" "security_group_rule_icmp_ipv6"
 #
 resource "openstack_networking_secgroup_rule_v2" "security_group_rule_nodes" {
 
-    for_each = var.security_group_rules
+  for_each = var.security_group_rules
 
-    direction         = each.value.security_group_rule_direction
-    ethertype         = each.value.security_group_rule_ethertype
-    protocol          = each.value.security_group_rule_protocol
-    port_range_min    = each.value.security_group_rule_port_range_min
-    port_range_max    = each.value.security_group_rule_port_range_max
-    remote_ip_prefix  = each.value.security_group_rule_remote_ip_prefix
-    security_group_id = openstack_networking_secgroup_v2.create_security_group.id
+  direction         = each.value.security_group_rule_direction
+  ethertype         = each.value.security_group_rule_ethertype
+  protocol          = each.value.security_group_rule_protocol
+  port_range_min    = each.value.security_group_rule_port_range_min
+  port_range_max    = each.value.security_group_rule_port_range_max
+  remote_ip_prefix  = each.value.security_group_rule_remote_ip_prefix
+  security_group_id = openstack_networking_secgroup_v2.create_security_group.id
 }
